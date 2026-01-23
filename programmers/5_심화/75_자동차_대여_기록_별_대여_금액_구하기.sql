7df8ad4b95c9be93acd2502e246b51f1a231ba51
jmjeon1004 <jmjeon1004@gmail.com>
2026-01-23 21:41:25 +0900

    [프로그래머스] 심화 - 자동차 대여 기록 별 대여 금액 구하기 (#75)

---
+++ programmers/5_심화/75_자동차_대여_기록_별_대여_금액_구하기.sql
@@ -0,0 +1,22 @@
+-- 자동차 대여 기록 별 대여 금액 구하기
+-- 프로그래머스 심화 (⭐⭐⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/151141
+-- 작성자: 전재민
+-- 작성일: 2026. 01. 23. 21:41:24
+
+SELECT 
+    H.HISTORY_ID,
+    FLOOR(C.DAILY_FEE * (DATEDIFF(H.END_DATE, H.START_DATE) + 1) * (1 - COALESCE(P.DISCOUNT_RATE, 0) / 100)) AS FEE
+FROM CAR_RENTAL_COMPANY_CAR C
+JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H ON C.CAR_ID = H.CAR_ID
+LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P ON C.CAR_TYPE = P.CAR_TYPE
+    AND P.DURATION_TYPE = (
+        CASE 
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 90 THEN '90일 이상'
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 30 THEN '30일 이상'
+            WHEN DATEDIFF(H.END_DATE, H.START_DATE) + 1 >= 7 THEN '7일 이상'
+            ELSE NULL 
+        END
+    )
+WHERE C.CAR_TYPE = '트럭'
+ORDER BY FEE DESC, H.HISTORY_ID DESC;
