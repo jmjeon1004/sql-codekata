7177fd932254bfcd7adbdff4f5bc8d03e87a6862
jmjeon1004 <jmjeon1004@gmail.com>
2026-07-11 13:45:03 +0900

    [프로그래머스] 입문 - 12세 이하인 여자 환자 목록 출력하기 (#28)

---
+++ programmers/1_입문/28_12세_이하인_여자_환자_목록_출력하기.sql
@@ -0,0 +1,21 @@
+-- 12세 이하인 여자 환자 목록 출력하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/132201
+-- 작성자: 전재민
+-- 작성일: 2026. 07. 11. 13:45:02
+
+SELECT
+    PT_NAME,
+    PT_NO,
+    GEND_CD,
+    AGE,
+    CASE
+        WHEN TLNO IS NULL THEN 'NONE'
+        ELSE TLNO
+    END AS TLNO
+FROM PATIENT
+WHERE GEND_CD = 'W'
+HAVING AGE <= 12
+ORDER BY 
+    AGE DESC,
+    PT_NAME ASC;
