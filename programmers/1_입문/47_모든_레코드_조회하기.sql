152510458b4553261818b9be7a7a1afc08961da6
jmjeon1004 <jmjeon1004@gmail.com>
2026-07-11 14:00:41 +0900

    [프로그래머스] 입문 - 모든 레코드 조회하기 (#47)

---
+++ programmers/1_입문/47_모든_레코드_조회하기.sql
@@ -0,0 +1,9 @@
+-- 모든 레코드 조회하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 작성자: 전재민
+-- 작성일: 2026. 07. 11. 14:00:40
+
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
