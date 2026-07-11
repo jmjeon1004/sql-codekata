91b4b68c97fea0897ace6913aac8c391661d7608
jmjeon1004 <jmjeon1004@gmail.com>
2026-07-11 15:34:19 +0900

    [LeetCode] 입문 - 1527. Patients With a Condition (#121)

---
+++ leetcode/1_입문/121_1527._Patients_With_a_Condition.sql
@@ -0,0 +1,9 @@
+-- 1527. Patients With a Condition
+-- LeetCode 입문 (⭐)
+-- 문제 링크: https://leetcode.com/problems/patients-with-a-condition/
+-- 작성자: 전재민
+-- 작성일: 2026. 07. 11. 15:34:18
+
+SELECT *
+FROM Patients
+WHERE conditions like ('%DIAB1%')
