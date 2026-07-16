b2c2bcb8a2f041f2e67ed9b74f1310c363f940d5
jmjeon1004 <jmjeon1004@gmail.com>
2026-07-16 19:31:28 +0900

    [LeetCode] 기초 - 197. Rising Temperature (#85)

---
+++ leetcode/2_기초/85_197._Rising_Temperature.sql
@@ -0,0 +1,11 @@
+-- 197. Rising Temperature
+-- LeetCode 기초 (⭐⭐)
+-- 문제 링크: https://leetcode.com/problems/rising-temperature/
+-- 작성자: 전재민
+-- 작성일: 2026. 07. 16. 19:31:27
+
+SELECT a.id
+FROM Weather as a
+JOIN Weather as b
+ON a.recordDate = b.recordDate+1
+where a.temperature > b.temperature
