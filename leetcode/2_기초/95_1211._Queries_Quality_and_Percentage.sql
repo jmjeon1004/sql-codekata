8ba21c8b2e6917c999f5d5d33788af39bf8b6370
jmjeon1004 <jmjeon1004@gmail.com>
2026-07-21 12:20:52 +0900

    [LeetCode] 기초 - 1211. Queries Quality and Percentage (#95)

---
+++ leetcode/2_기초/95_1211._Queries_Quality_and_Percentage.sql
@@ -0,0 +1,12 @@
+-- 1211. Queries Quality and Percentage
+-- LeetCode 기초 (⭐⭐)
+-- 문제 링크: https://leetcode.com/problems/queries-quality-and-percentage/
+-- 작성자: 전재민
+-- 작성일: 2026. 07. 21. 12:20:51
+
+SELECT
+    query_name,
+    round(avg(rating/position),2) AS quality,
+    round((1/count(result))*100,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
